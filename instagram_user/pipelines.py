4267574545fc7b6db3739e2c5310023c17d063f4
kengsley1993 <danny108735@gmail.com>
2019-12-27 13:37:39 +0800

    update the FilePipeline - able to handle mulitple username and store in different user's folder

--- instagram_user/pipelines.py
+++ instagram_user/pipelines.py
@@ -15,7 +15,10 @@ from instagram_user import settings
 
 class FilePipeline(FilesPipeline):
 	def file_path(self, request, response=None, info=None):
-		file_name = settings.USERNAME + '/' + os.path.basename(urlparse(request.url).path)
+		# use the meta store in get_media_requests and get the item
+		item = request.meta['item']
+		# use username in item to dirct the path
+		file_name = item['username'] +'/'+ os.path.basename(urlparse(request.url).path)
 		return file_name
 	
 	def item_completed(self, results, item, info):
@@ -26,9 +29,12 @@ class FilePipeline(FilesPipeline):
 	
 	def get_media_requests(self, item, info):
 		for url in item['image_list'].split(';'):
-			yield Request(url)
+			if url is not '':
+				# Add item with meta into request for following step
+				yield Request(url, meta={'item': item})
 		for url in item['video_list'].split(';'):
-			yield Request(url)
+			if url is not '':
+				yield Request(url, meta={'item': item})
 
 class InstagramUserPipeline(object):
     def process_item(self, item, spider):

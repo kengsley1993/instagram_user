3cbf34c495a22b69971c5d784996e183d71aa0f0
user_test_001 <test001@user.com>
2020-01-04 22:23:06 +0800

    add data save in SQL

--- instagram_user/pipelines.py
+++ instagram_user/pipelines.py
@@ -12,6 +12,42 @@ from scrapy import Request
 from scrapy.exceptions import DropItem
 from scrapy.pipelines.files import FilesPipeline
 from instagram_user import settings
+import pymysql
+
+class MysqlPipeline():
+	def __init__(self, host, database, user, password, port):
+		self.host = host
+		self.database = database
+		self.user = user
+		self.password = password
+		self.port = port
+
+	@classmethod
+	def from_crawler(cls, crawler):
+		return cls(
+			host=crawler.settings.get('MYSQL_HOST'),
+			database=crawler.settings.get('MYSQL_DATABASE'),
+			user=crawler.settings.get('MYSQL_USER'),
+			password=crawler.settings.get('MYSQL_PASSWORD'),
+			port=crawler.settings.get('MYSQL_PORT'),
+		)
+
+	def open_spider(self, spider):
+		self.db = pymysql.connect(self.host, self.user, self.password, self.database, charset='utf8mb4', port=self.port)
+		self.cursor = self.db.cursor()
+
+	def close_spider(self, spider):
+		self.db.close()
+
+	def process_item(self, item, spider):
+		data = dict(item)
+		keys = ', '.join(data.keys())
+		values = ', '.join(['%s']*len(data))
+		insert_sql = 'insert into %s (%s) values (%s)' % (item['username'], keys, values)
+		# update_sql = 'update %s set %s=%s where %s=%s'
+		self.cursor.execute(insert_sql, tuple(data.values()))
+		self.db.commit()
+		return item
 
 class FilePipeline(FilesPipeline):
 	def file_path(self, request, response=None, info=None):
